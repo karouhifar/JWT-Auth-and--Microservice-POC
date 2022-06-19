a13699914c3ce4e936be41403d96b074c26eb569
kamyab rouhifar <krouhifar@myseneca.ca>
2022-06-18 22:35:39 -0400

    added helmet

--- functions/src/index.js
+++ functions/src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
 const routes = require('./routes');
 require('dotenv').config();
 const app = express();
 
+app.use(helmet());
+app.use(cors({ origin: true }));
 // const HTTP_PORT = parseInt(process.env.PORTS || 8080, 10);
 
 app.use('/v1', routes);
