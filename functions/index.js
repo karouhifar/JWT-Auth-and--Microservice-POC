b9ea68e3b22177489a274f89826a9295996fef29
kamyab rouhifar <krouhifar@myseneca.ca>
2022-06-18 20:37:36 -0400

    set firebase cloud

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,8 @@ const functions = require('firebase-functions');
 const app = require('./src');
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.microservice = functions.https.onRequest(app);
+exports.microservice = functions.https.onRequest((request, response) => {
+    request.url = `/v1${request.url}`; // prepend '/v1' to keep query params if any
+    functions.logger.info(request.path);
+    return app(request, response);
+});
